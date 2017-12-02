3c892624429afb3c01c3b3058074e8451bfb931b
Martin Vilcans <martin@resolutiongames.com>
2017-12-02 21:15:41 +0100

    Add missing Box.cs

---
+++ Assets/LD40/Box.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+    void OnCollisionEnter2D(Collision2D collision) {
+        Debug.LogFormat("{0} collided with {1}", this, collision.gameObject);
+        if(collision.gameObject.name.StartsWith("Ground")) {
+            Destroy(gameObject);
+        }
+    }
+}
