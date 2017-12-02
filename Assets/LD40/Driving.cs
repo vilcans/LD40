e6fa21e697cb96945dcb39b24fc51470c70b76a7
Martin Vilcans <martin@resolutiongames.com>
2017-12-02 15:55:34 +0100

    Tweak physics and add sound

--- Assets/LD40/Driving.cs
+++ Assets/LD40/Driving.cs
@@ -1,33 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Driving : MonoBehaviour {
-    private float speedFactor = 500.0f;
-    private float rotationFactor = 900f;
+    public float maxTorque = 10000;
+    public float maxSpeed = 500.0f;
+    public float rotationSpeed = 900f;
+    private float acceleration = 5;
 
     private WheelJoint2D[] wheels;
 
-    private float speed;
-
     private Rigidbody2D body;
+
+    private float currentSpeed;
+
+    private AudioSource audio;
+    private float idleAudioPitch = .8f;
+    private float maxSpeedPitch = 1.5f;
 
     private void Awake() {
         wheels = GetComponentsInChildren<WheelJoint2D>();
         body = GetComponent<Rigidbody2D>();
+        audio = GetComponent<AudioSource>();
     }
 
-    private void Update() {
-        speed = Input.GetAxis("Vertical") * -speedFactor;
-        JointMotor2D motor = new JointMotor2D {
-            motorSpeed = speed,
-            maxMotorTorque = 100000
-        };
-        for(int i = 0, len = wheels.Length; i < len; ++i) {
-            wheels[i].motor = motor;
+    private void FixedUpdate() {
+        float wantedSpeed = Input.GetAxis("Vertical") * -maxSpeed;
+        currentSpeed = Mathf.MoveTowards(currentSpeed, wantedSpeed, acceleration);
+
+        if(false) {
+            for(int i = 0, len = wheels.Length; i < len; ++i) {
+                wheels[i].useMotor = false;
+            }
+        }
+        else {
+            JointMotor2D motor = new JointMotor2D {
+                motorSpeed = currentSpeed,
+                maxMotorTorque = maxTorque
+            };
+            for(int i = 0, len = wheels.Length; i < len; ++i) {
+                wheels[i].motor = motor;
+            }
+
         }
 
-        float rotation = Input.GetAxis("Horizontal") * -rotationFactor;
+        audio.pitch = Mathf.LerpUnclamped(idleAudioPitch, maxSpeedPitch, Mathf.Abs(currentSpeed) / maxSpeed);
+
+        float rotation = Input.GetAxis("Horizontal") * -rotationSpeed;
         body.AddTorque(rotation);
     }
 }
