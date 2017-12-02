406998852f02c7b5e2f293fb940b9fae7690d844
Martin Vilcans <martin@resolutiongames.com>
2017-12-02 14:45:09 +0100

    Possible to rotate

--- Assets/LD40/Driving.cs
+++ Assets/LD40/Driving.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Driving : MonoBehaviour {
-    public float speedFactor = 500.0f;
+    private float speedFactor = 500.0f;
+    private float rotationFactor = 900f;
 
     private WheelJoint2D[] wheels;
 
     private float speed;
 
+    private Rigidbody2D body;
+
     private void Awake() {
         wheels = GetComponentsInChildren<WheelJoint2D>();
+        body = GetComponent<Rigidbody2D>();
     }
 
     private void Update() {
         speed = Input.GetAxis("Vertical") * -speedFactor;
-        Debug.LogFormat("speed {0}", speed);
         JointMotor2D motor = new JointMotor2D {
             motorSpeed = speed,
             maxMotorTorque = 100000
@@ -23,5 +26,8 @@ public class Driving : MonoBehaviour {
         for(int i = 0, len = wheels.Length; i < len; ++i) {
             wheels[i].motor = motor;
         }
+
+        float rotation = Input.GetAxis("Horizontal") * -rotationFactor;
+        body.AddTorque(rotation);
     }
 }
