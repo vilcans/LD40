a280a58dae6917c2e0adda4fb6c3a3282ba883b3
Martin Vilcans <martin@resolutiongames.com>
2017-12-02 14:33:05 +0100

    Possible to drive

---
+++ Assets/LD40/Driving.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driving : MonoBehaviour {
+    public float speedFactor = 500.0f;
+
+    private WheelJoint2D[] wheels;
+
+    private float speed;
+
+    private void Awake() {
+        wheels = GetComponentsInChildren<WheelJoint2D>();
+    }
+
+    private void Update() {
+        speed = Input.GetAxis("Vertical") * -speedFactor;
+        Debug.LogFormat("speed {0}", speed);
+        JointMotor2D motor = new JointMotor2D {
+            motorSpeed = speed,
+            maxMotorTorque = 100000
+        };
+        for(int i = 0, len = wheels.Length; i < len; ++i) {
+            wheels[i].motor = motor;
+        }
+    }
+}
