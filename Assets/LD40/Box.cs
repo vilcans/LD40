664bbff31028f12ad37531ceff24395529041039
Martin Vilcans <martin@resolutiongames.com>
2017-12-03 12:29:07 +0100

    Particles when packages break

--- Assets/LD40/Box.cs
+++ Assets/LD40/Box.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Box : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D collision) {
         //Debug.LogFormat("{0} collided with {1}", this, collision.gameObject);
         if(collision.gameObject.name.StartsWith("Ground")) {
+            GameObject particles = Instantiate(GameSettings.instance.destroyEffect);
+            particles.transform.position = transform.position;
             Destroy(gameObject);
         }
     }
---
+++ Assets/LD40/GameSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour {
+    public static GameSettings instance;
+
+    public GameObject destroyEffect;
+
+    public void Awake() {
+        instance = this;
+    }
+}
