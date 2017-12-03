d284e6715bed1edf449a95fd7a88792fecdb2651
Martin Vilcans <martin@resolutiongames.com>
2017-12-03 22:24:32 +0100

    Cycle background color

---
+++ Assets/LD40/Game.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour {
+    public Texture2D colors;
+
+    private float startTime;
+
+    private new Camera camera;
+
+    void Awake() {
+        camera = Camera.main;
+        startTime = Time.time;
+    }
+
+    void Update() {
+        float time = Time.time - startTime;
+
+        float v = 1 - Mathf.PingPong(time / 60.0f, 1);
+        Color c = colors.GetPixelBilinear(0.5f, v);
+        camera.backgroundColor = c;
+    }
+}
