1395d11598f63b9dddc8bc9e2065c4523113a37c
Martin Vilcans <martin@resolutiongames.com>
2017-12-03 17:38:25 +0100

    Use layer for checking hit with ground

--- Assets/LD40/Box.cs
+++ Assets/LD40/Box.cs
@@ -4,7 +4,7 @@ public class Box : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D collision) {
         //Debug.LogFormat("{0} collided with {1}", this, collision.gameObject);
-        if(collision.gameObject.name.StartsWith("Ground")) {
+        if(collision.gameObject.layer == 8) {
             GameObject particles = Instantiate(GameSettings.instance.destroyEffect);
             particles.transform.position = transform.position;
             AudioClip[] availableClips = GameSettings.instance.hitClips;
