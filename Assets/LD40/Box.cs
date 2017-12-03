590f0109af31219611c13e4149a53ef01549428b
Martin Vilcans <martin@resolutiongames.com>
2017-12-03 12:03:09 +0100

    Remove collision log

--- Assets/LD40/Box.cs
+++ Assets/LD40/Box.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Box : MonoBehaviour {
     void OnCollisionEnter2D(Collision2D collision) {
-        Debug.LogFormat("{0} collided with {1}", this, collision.gameObject);
+        //Debug.LogFormat("{0} collided with {1}", this, collision.gameObject);
         if(collision.gameObject.name.StartsWith("Ground")) {
             Destroy(gameObject);
         }
