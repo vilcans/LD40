4e8e2c145cbb2b14d9c396a607334e654b796187
Martin Vilcans <martin@resolutiongames.com>
2017-12-03 17:43:07 +0100

    Wait 0.1 seconds before blowing up package

--- Assets/LD40/Box.cs
+++ Assets/LD40/Box.cs
@@ -2,9 +2,29 @@
 
 public class Box : MonoBehaviour {
 
-    void OnCollisionEnter2D(Collision2D collision) {
-        //Debug.LogFormat("{0} collided with {1}", this, collision.gameObject);
+    private int numberOfCollisions;
+    private float timeInCollision;
+
+    private void OnCollisionExit2D(Collision2D collision) {
         if(collision.gameObject.layer == 8) {
+            --numberOfCollisions;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.layer == 8) {
+            ++numberOfCollisions;
+        }
+    }
+
+    void Update() {
+        if(numberOfCollisions == 0) {
+            timeInCollision = 0;
+            return;
+        }
+        timeInCollision += Time.deltaTime;
+        //Debug.LogFormat("{0} collided with {1}", this, collision.gameObject);
+        if(timeInCollision > .1f) {
             GameObject particles = Instantiate(GameSettings.instance.destroyEffect);
             particles.transform.position = transform.position;
             AudioClip[] availableClips = GameSettings.instance.hitClips;
